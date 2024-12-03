bf02358a70f9619c8a7ba7b9b5acf37df09f8c1b
Fab192 <framirez90304@ufide.ac.cr>
2024-12-02 19:33:39 -0600

    Reseñas

---
+++ ReseñasRestaurantes/src/main/java/com/example/ResenasRestaurantes/ResenasRestaurantesApplication.java
@@ -0,0 +1,13 @@
+package com.example.ResenasRestaurantes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResenasRestaurantesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResenasRestaurantesApplication.class, args);
+	}
+
+}
